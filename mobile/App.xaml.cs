623e07c59d496aa8d4ade30ac7d8e463a7980565
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-04-02 20:02:18 +0200

    chore: create maui-blazor hybrid project

---
+++ mobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TelkoMomentApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ mobile/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TelkoMomentApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
